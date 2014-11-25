4c47272dfec29a1c36abbe6b1735013a5a5e5ede
Drasko Gomboc <drasko.gomboc@gmail.com>
2014-11-25 13:59:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,71 @@
+geoip
+======
+
+> geoip - [php](http://php.net) library
+
+## Install
+Via Composer
+
+```sh
+composer require g4/geoip
+```
+
+## Usage
+
+``` php
+<?php
+    
+$geoIp = new \G4\GeoIp\GeoIp('127.0.0.1');
+
+// "area_code" -- The PSTN area code (ex: 212)
+echo $geoIp->getAreaCode();
+
+// "dma_code" -- Designated Market Area code (USA and Canada only)
+echo $geoIp->getDmaCode();
+
+// "city" -- The city
+echo $geoIp->getCity();
+
+// "continent_code" -- Two letter continent code
+echo $geoIp->getContinentCode();
+
+// "country_code" -- Two letter country code
+echo $geoIp->getCountryCode();
+
+// "country_code3" -- Three letter country code
+echo $geoIp->getCountryCode3();
+
+// "country_name" -- The country name
+echo $geoIp->getCountryName();
+echo $geoIp->getIp();
+
+// "latitude" -- The Latitude as signed double
+echo $geoIp->getLatitude();
+
+// "longitude" -- The Longitude as signed double
+echo $geoIp->getLongitude();
+
+// "postal_code" -- The Postal Code, FSA or Zip Code
+echo $geoIp->getPostalCode();
+
+// "region" -- The region code (ex: CA for California)
+echo $geoIp->getRegion();
+
+[geoip_record_by_name](http://php.net/manual/en/function.geoip-record-by-name.php)
+
+```
+
+## Development
+
+### Install dependencies
+
+    $ make install
+
+### Run tests
+
+    $ make test
+
+## License
+
+(The MIT License)
+see LICENSE file for details...
---
+++ src/Geoip/GeoIp.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace G4\GeoIP;
+
+class GeoIP
+{
+    const ENCODING_ISO = 'ISO-8859-1';
+    const ENCODING_UTF = 'UTF-8';
+
+    /**
+     * @var string
+     */
+    private $ip;
+
+    /**
+     * @var array
+     */
+    private $record;
+
+    /**
+     * @param $ip
+     */
+    public function __construct($ip = null)
+    {
+        $this->ip = $ip;
+        $this->initialize();
+    }
+
+    /**
+     * Area code (phone)
+     * @return int
+     */
+    public function getAreaCode()
+    {
+        return $this->get('area_code');
+    }
+
+    /**
+     * @return int
+     */
+    public function getDmaCode()
+    {
+        return $this->get('dma_code');
+    }
+
+    /**
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->convert($this->get('city'));
+    }
+
+    /**
+     * @return string
+     */
+    public function getContinentCode()
+    {
+        return $this->get('continent_code');
+    }
+
+    /**
+     * Two letter country representation
+     * @return string
+     */
+    public function getCountryCode()
+    {
+        return $this->get('country_code');
+    }
+
+    /**
+     * Three letter country representation
+     * @return string
+     */
+    public function getCountryCode3()
+    {
+        return $this->get('country_code3');
+    }
+
+    /**
+     * @return string
+     */
+    public function getCountryName()
+    {
+        return $this->convert($this->get('country_name'));
+    }
+
+    /**
+     * @return string
+     */
+    public function getIp()
+    {
+        return $this->ip;
+    }
+
+    /**
+     * @return double
+     */
+    public function getLatitude()
+    {
+        return $this->get('latitude');
+    }
+
+    /**
+     * @return double
+     */
+    public function getLongitude()
+    {
+        return $this->get('longitude');
+    }
+
+    /**
+     * @return int
+     */
+    public function getPostalCode()
+    {
+        return $this->get('postal_code');
+    }
+
+    /**
+     * @return int
+     */
+    public function getRegion()
+    {
+        return $this->convert($this->get('region'));
+    }
+
+    /**
+     * @return boolean
+     */
+    public function hasCity()
+    {
+        return $this->get('city') !== null;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function hasCountryCode()
+    {
+        return $this->get('country_code') !== null;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function hasRecord()
+    {
+        return is_array($this->record) && !empty($this->record);
+    }
+
+    private function convert($value)
+    {
+        return mb_convert_encoding($value, self::ENCODING_UTF, self::ENCODING_ISO);
+    }
+
+    /**
+     * @param string $key
+     * @return bool
+     */
+    private function get($key)
+    {
+        return $this->hasRecord() ? $this->record[$key] : null;
+    }
+
+    private function initialize()
+    {
+        $this->record = @geoip_record_by_name($this->ip);
+    }
+}
